1a899628efbbe3f1374bf0fbd52668fa7ade760a
raha1388 <139645101+raha1388@users.noreply.github.com>
2023-07-23 12:07:34 +0330

    Add files via upload

---
+++ raha ghayebi.py
@@ -0,0 +1,13 @@
+import random
+
+com_namber=random.randint(1,6)
+print(com_namber)
+
+if com_namber==(1,5):
+    print(com_namber)
+
+elif com_namber==(6):
+    user_number=random.randint(1,6)
+    print(user_number)
+
+
---
+++ raha ghayebi2.py
@@ -0,0 +1,5 @@
+import math
+sanie=9000
+adad=60
+daghighe=sanie/adad
+print(daghighe)
